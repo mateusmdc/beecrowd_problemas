79bb04452d42de038299b908c9f31b89b3b0bc9e
Mateus Matos <38740703+mateusmdc@users.noreply.github.com>
2024-06-27 15:15:45 -0300

    beecrowd starter problem 1002 solved in javascript

---
+++ Iniciante/JavaScript/1002.js
@@ -0,0 +1,9 @@
+// This line is necessary to read a file to get the user input, is the filepath that Beecrowd uses
+// When executing the code locally Remeber to:
+//  -> remove the . before /dev and 'txt' after stdin when submitting to beecrowd (or when uploading to github, but that is optional)
+var input = require('fs').readFileSync('./dev/stdin.txt', 'utf8');
+var lines = input.split('\n');
+
+var r = parseFloat(lines.shift())
+
+console.log(`A=${((r**2)*3.14159).toFixed(4)}`)
