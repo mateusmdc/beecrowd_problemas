edd356133d199a0d7380162cb5a9a1f03e423fff
Mateus Matos <38740703+mateusmdc@users.noreply.github.com>
2024-06-27 16:19:22 -0300

    beecrowd starter problem 1010 solved in javascript

---
+++ Iniciante/JavaScript/1010.js
@@ -0,0 +1,15 @@
+// This line is necessary to read a file to get the user input, is the filepath that Beecrowd uses
+// When executing the code locally Remeber to:
+//  -> remove the . before /dev and 'txt' after stdin when submitting to beecrowd (or when uploading to github, but that is optional)
+var input = require('fs').readFileSync('./dev/stdin.txt', 'utf8');
+var lines = input.split('\n');
+
+var values = []
+
+lines.forEach(l => {
+    values.push(l.trim().split(" "))
+})
+
+const [a,b,c,d,e,f] = [...values[0], ...values[1]]
+
+console.log(`VALOR A PAGAR: R$ ${((b*c)+(e*f)).toFixed(2)}`)
