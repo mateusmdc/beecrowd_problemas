2e2fe15a9b49e9a93433a6f22e15834215efcd0d
Mateus Matos <38740703+mateusmdc@users.noreply.github.com>
2024-06-27 15:20:24 -0300

    beecrowd starter problem 1003 solved in javascript

---
+++ Iniciante/JavaScript/1003.js
@@ -0,0 +1,10 @@
+// This line is necessary to read a file to get the user input, is the filepath that Beecrowd uses
+// When executing the code locally Remeber to:
+//  -> remove the . before /dev and 'txt' after stdin when submitting to beecrowd (or when uploading to github, but that is optional)
+var input = require('fs').readFileSync('./dev/stdin.txt', 'utf8');
+var lines = input.split('\n');
+
+var a = parseInt(lines.shift());
+var b = parseInt(lines.shift());
+
+console.log(`SOMA = ${a+b}`);
