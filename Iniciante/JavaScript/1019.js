4a96b8138e7ad46aeb1bdee28538f7548f55020f
Mateus Matos <38740703+mateusmdc@users.noreply.github.com>
2024-06-28 15:53:54 -0300

    fixed problem 1019 code

--- Iniciante/JavaScript/1019.js
+++ Iniciante/JavaScript/1019.js
@@ -4,4 +4,6 @@
 var input = require('fs').readFileSync('./dev/stdin.txt', 'utf8');
 var lines = input.trim().split('\n');
 
+lines = parseInt(lines);
+
 console.log(`${Math.floor(lines/3600)}:${Math.floor((lines%3600)/60)}:${Math.floor((lines%3660)%60)}`);
